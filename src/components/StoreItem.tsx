8689c34461e89772e5a557e669b1709d93f9a061
Jerom Miole <miolejerom@gmail.com>
2023-09-03 23:35:17 +1200

    new: utilities currency

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
 import { Card } from "react-bootstrap"
+import { formatCurrency } from "../utilities/formatCurrency"
 
 type StoreItemProps = {
     id: number
@@ -16,10 +17,9 @@ StoreItemProps) {
             style={{objectFit: "cover"}}
         />
         <Card.Body className="d-flex flex-column">
-            <Card.Title className="d-flex
-            justify-content-space-between align-items-baseline mb-4">
+            <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
                 <span className="fs-2">{name}</span>
-                <span className="ms-2 text-muted">{price}</span>
+                <span className="ms-2 text-muted">{formatCurrency(price)}</span>
             </Card.Title>
         </Card.Body>
     </Card>
