d1bebe69923ed240e7a161b3db86e1b0f2fb8a4d
Jerom Miole <miolejerom@gmail.com>
2023-09-06 15:37:26 +1200

    feat: update images

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,40 @@
+import { Button, Col, Container, Image, Row } from "react-bootstrap"
+import HomeImage1 from "../../public/imgs/7.png"
+import HomeImage2 from "../../public/imgs/9.png"
+import HomeImage3 from "../../public/imgs/1.png"
+
 export function Home() {
-    return <h1>Home</h1>
+    return (
+        <>
+            <div>
+                <Container>
+                    <Row className="mt-5">
+                        <Col>
+                            <Image src={HomeImage3} style={{
+                                width: "100%",
+                            }}/>
+                        </Col>
+                        <Col>
+                            <Image src={HomeImage2} style={{
+                                width: "100%",
+                            }}/>
+                        </Col>
+                        <Col>
+                            <Image src={HomeImage1} style={{
+                                width: "100%",
+                            }}/>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <div className="pt-5">
+                            <h2 className="text-light">React TypeScript</h2>
+                            <h1 className="text-light">Shopping Cart</h1>
+                            <br />
+                            <Button href="/store" className="btn btn-light">Go to Store</Button>
+                        </div>
+                    </Row>
+                </Container>
+            </div>
+        </>
+    )
 }
